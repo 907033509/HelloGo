16e9ce282c6b75c0d4bd5da0742bbf972a0d7bd5
MrSong <907033509@QQ.com>
2021-07-23 11:54:21 +0800

    变量申明

--- QianFengStudy/VarStudy/var1.go
+++ QianFengStudy/VarStudy/var1.go
@@ -15,6 +15,7 @@ func main(){
 			第三种：简短声明，省略var
 				变量名 := 赋值
 		step2：变量的访问，也叫作赋值和取值
+			直接根据变量名访问
 	*/
 	
 	//第一种：定义变量，然后赋值
@@ -28,4 +29,24 @@ func main(){
 	//第三种：剪短定义，也叫作简短声明
 	sum1 := 100
 	fmt.Println("sum1的数值是：%d\n",sum1)
+
+	//多个变量同时定义
+	var a,b,c int
+	a = 1
+	b = 2
+	c = 3
+	fmt.Println(a,b,c)
+
+	var m,n int = 100,200
+	fmt.Println(m,n)
+
+	var b1,n1,m1 = 1,2,"lihua"
+	fmt.Println(b1,n1,m1)
+
+	var (
+		name = "李晓华"
+		age = 18
+		sex = "女"
+	)
+	fmt.Println(name,age,sex)
 }
